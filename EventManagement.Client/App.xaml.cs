e5fcb2d265aec199d2a9701e4ba5af6d35e2fd66
Zoran Pantos <pantos78zoran@gmail.com>
2022-11-10 15:41:35 +0100

    Add project files.

---
+++ EventManagement.Client/App.xaml.cs
@@ -0,0 +1,5 @@
+﻿using System.Windows;
+
+namespace EventManagement.Client;
+
+public partial class App : Application { }
---
+++ EventManagement.Client/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace EventManagement.Client;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
