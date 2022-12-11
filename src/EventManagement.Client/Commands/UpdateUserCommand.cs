68b4ee26e213b8ed827167c0a045c8d3aeef0d83
Zoran Pantos <pantos78zoran@gmail.com>
2022-12-11 13:30:18 +0100

    feat: implement CanExecute method on UpdateUserCommand

--- src/EventManagement.Client/Commands/UpdateUserCommand.cs
+++ src/EventManagement.Client/Commands/UpdateUserCommand.cs
@@ -1,5 +1,6 @@
 ﻿using EventManagement.Demo.Infrastructure.Repositories;
 using EventManagement.Demo.ViewModels;
+using System.ComponentModel;
 
 namespace EventManagement.Demo.Commands;
 
@@ -14,11 +15,29 @@ public class UpdateUserCommand : CommandBase
     {
         this.updateUserViewModel = updateUserViewModel;
         this.eventManagementRepository = eventManagementRepository;
+
+        updateUserViewModel.PropertyChanged += OnViewModelPropertyChanged;
+    }
+
+    private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName.Equals(nameof(UpdateUserViewModel.FirstName)) ||
+            e.PropertyName.Equals(nameof(UpdateUserViewModel.LastName)))
+        {
+            OnCanExecuteChanged();
+        }
     }
 
     public override void Execute(object? parameter)
     {
         // TODO: get this user id from some upper layers
         eventManagementRepository.UpdateUserWithId(2, updateUserViewModel);
+    }
+
+    public override bool CanExecute(object? parameter)
+    {
+        return !string.IsNullOrEmpty(updateUserViewModel.FirstName) &&
+            !string.IsNullOrEmpty(updateUserViewModel.LastName) &&
+            base.CanExecute(parameter);
     }
 }
