0838c1d5c50cbca79fbd65a9c355f2e5d7724e21
Zoran Pantos <pantos78zoran@gmail.com>
2022-12-11 18:11:52 +0100

    chore: add commands to the main view model

--- src/EventManagement.Client/ViewModels/MainViewModel.cs
+++ src/EventManagement.Client/ViewModels/MainViewModel.cs
@@ -1,11 +1,16 @@
-﻿using EventManagement.Demo.Infrastructure;
-using EventManagement.Demo.Infrastructure.Repositories;
+﻿using EventManagement.Demo.Infrastructure.Repositories;
+using System.Windows.Input;
 
 namespace EventManagement.Demo.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
     public ViewModelBase CurrentViewModel { get; }
+
+    public ICommand HomeCommand { get; }
+    public ICommand MyGroupsCommand { get; }
+    public ICommand MyApplicationsCommand { get; }
+    public ICommand ProfileCommand { get; }
 
     // Temporarily hardcoded
     public MainViewModel() => CurrentViewModel = new UserInfoViewModel(new EventManagementRepository());
