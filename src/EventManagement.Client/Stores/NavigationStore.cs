b0fc7a6300264f200f4d3fa661b60427ddeb45cc
Zoran Pantos <pantos78zoran@gmail.com>
2022-12-12 10:28:58 +0100

    feat: add initial navigation store class

---
+++ src/EventManagement.Client/Stores/NavigationStore.cs
@@ -0,0 +1,17 @@
+﻿using EventManagement.Demo.ViewModels;
+
+namespace EventManagement.Demo.Stores;
+
+public class NavigationStore
+{
+    private ViewModelBase currentViewModel;
+
+    public ViewModelBase CurrentViewModel
+    {
+        get => currentViewModel;
+        set
+        {
+            currentViewModel = value;
+        }
+    }
+}
