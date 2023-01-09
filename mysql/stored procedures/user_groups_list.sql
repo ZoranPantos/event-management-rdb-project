64852f8be55ad92fa19909315f42c8f465c173c8
Zoran Pantos <pantos78zoran@gmail.com>
2023-01-09 15:40:48 +0100

    chore: update stored procedure, view and trigger

--- mysql/stored procedures/user_groups_list.sql
+++ mysql/stored procedures/user_groups_list.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `user_groups_list`(IN userId INT)
+CREATE PROCEDURE user_groups_list (IN userId INT)
 BEGIN
 	SELECT * FROM event_management.`group` g
     WHERE g.REGULAR_USER_USER_Id=userId;
--- mysql/views/forthcoming_events_view.sql
+++ mysql/views/forthcoming_events_view.sql
@@ -1,22 +1,7 @@
-CREATE 
-    ALGORITHM = UNDEFINED 
-    DEFINER = `root`@`localhost` 
-    SQL SECURITY DEFINER
-VIEW `event_management`.`forthcoming_events_view` AS
-    SELECT 
-        `e`.`Id` AS `Id`,
-        `e`.`Title` AS `Title`,
-        `e`.`Date` AS `Date`,
-        `e`.`Description` AS `Description`,
-        `e`.`DailySchedule` AS `DailySchedule`,
-        `l`.`City` AS `City`,
-        `l`.`Street` AS `Street`,
-        `l`.`Number` AS `Number`,
-        `g`.`Title` AS `GroupName`
-    FROM
-        (((`event_management`.`event` `e`
-        JOIN `event_management`.`location` `l` ON ((`e`.`LOCATION_Id` = `l`.`Id`)))
-        JOIN `event_management`.`organizes` `o` ON ((`o`.`EVENT_Id` = `e`.`Id`)))
-        JOIN `event_management`.`group` `g` ON ((`o`.`GROUP_Id` = `g`.`Id`)))
-    WHERE
-        (`e`.`Date` > CURDATE())
+CREATE VIEW forthcoming_events_view AS
+SELECT e.Id, e.Title, e.`Date`, e.`Description`, DailySchedule, City, Street, `Number`, g.Title AS GroupName
+FROM event_management.`event` e 
+  INNER JOIN event_management.location l ON e.LOCATION_Id=l.Id
+  INNER JOIN event_management.organizes o ON o.EVENT_Id=e.Id
+  INNER JOIN event_management.`group` g ON o.GROUP_Id=g.Id
+WHERE e.`Date`>CURRENT_DATE();
